8e8c818231a2f5a1963913547d6b3dbc513d7723
John Cardozo <johncardozo@gmail.com>
2023-08-15 18:23:22 -0500

    Angular: Transacciones por parámetro al componente Transactions

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IBalance } from './models/balance.model';
+import { ITransaction } from './models/transaction.model';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,27 @@ export class AppComponent {
     income: 100_000,
     expenses: 45_000,
   };
+  transactions: ITransaction[] = [
+    {
+      id: '1',
+      type: 'expense',
+      amount: 5_000,
+      category: 'utilities',
+      date: new Date(2023, 8, 10),
+    },
+    {
+      id: '2',
+      type: 'expense',
+      amount: 15_000,
+      category: 'food',
+      date: new Date(2023, 8, 11),
+    },
+    {
+      id: '3',
+      type: 'income',
+      amount: 80_000,
+      category: 'payroll',
+      date: new Date(2023, 8, 12),
+    },
+  ];
 }
--- angular/src/app/components/transactions/transactions.component.ts
+++ angular/src/app/components/transactions/transactions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ITransaction } from 'src/app/models/transaction.model';
 
 @Component({
@@ -7,27 +7,5 @@ import { ITransaction } from 'src/app/models/transaction.model';
   styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent {
-  transactions: ITransaction[] = [
-    {
-      id: '1',
-      type: 'expense',
-      amount: 5_000,
-      category: 'utilities',
-      date: new Date(2023, 8, 10),
-    },
-    {
-      id: '2',
-      type: 'expense',
-      amount: 15_000,
-      category: 'food',
-      date: new Date(2023, 8, 11),
-    },
-    {
-      id: '3',
-      type: 'income',
-      amount: 80_000,
-      category: 'payroll',
-      date: new Date(2023, 8, 12),
-    },
-  ];
+  @Input() transactions!: ITransaction[];
 }
