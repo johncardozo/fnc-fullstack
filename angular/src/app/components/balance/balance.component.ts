6fb8dceefff2bbb5bcd1c538711a13497c6bc3d9
John Cardozo <johncardozo@gmail.com>
2023-08-14 20:02:20 -0500

    Angular: Creación del proyecto y del componente Balance

---
+++ angular/src/app/components/balance/balance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance',
+  templateUrl: './balance.component.html',
+  styleUrls: ['./balance.component.scss'],
+})
+export class BalanceComponent {
+  balance = 150_000;
+  search = 'Celestron';
+}
