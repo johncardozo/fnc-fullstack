db3602aa20b346493c2ad1b30177bc3a47e7c489
John Cardozo <johncardozo@gmail.com>
2023-08-24 18:36:53 -0500

    React Native: Acceso a los atributos de los objetos del arreglo en el Map

--- coffee-network/src/components/CoffeeList.jsx
+++ coffee-network/src/components/CoffeeList.jsx
@@ -6,7 +6,15 @@ const CoffeeList = () => {
     <View>
       {coffees.map((coffee) => (
         <View key={coffee.id}>
+          <Text>{coffee.id}</Text>
           <Text>{coffee.product}</Text>
+          <Text>{coffee.brand}</Text>
+          <Text>{coffee.country}</Text>
+          <Text>{coffee.body}</Text>
+          <Text>{coffee.acidity}</Text>
+          <Text>{coffee.notes}</Text>
+          <Text>{coffee.stars}</Text>
+          <Text>--------------</Text>
         </View>
       ))}
     </View>
