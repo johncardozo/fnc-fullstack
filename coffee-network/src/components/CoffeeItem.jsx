09f7ee1d3fee3130104c246fdf53e001892bd3bc
John Cardozo <johncardozo@gmail.com>
2023-09-05 17:35:05 -0500

    React Native: Estilos de CoffeeList y CoffeeItem

--- coffee-network/src/components/CoffeeItem.jsx
+++ coffee-network/src/components/CoffeeItem.jsx
@@ -2,6 +2,8 @@ import { Text, View, StyleSheet } from "react-native";
 import StyledText from "./StyledText";
 import CoffeeItemHeader from "./CoffeeItemHeader";
 
+import theme from "../theme";
+
 const CoffeeItem = ({ coffee }) => {
   return (
     <View style={styles.container}>
@@ -19,6 +21,12 @@ const CoffeeItem = ({ coffee }) => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    backgroundColor: theme.colors.cardBackground,
+    marginTop: 6,
+    marginBottom: 6,
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 15,
   },
   product: {
     fontWeight: "900",
--- coffee-network/src/components/CoffeeList.jsx
+++ coffee-network/src/components/CoffeeList.jsx
@@ -1,16 +1,25 @@
-import { Text, FlatList } from "react-native";
+import { Text, FlatList, StyleSheet } from "react-native";
 
 import { coffees } from "../data/coffees";
 import CoffeeItem from "./CoffeeItem";
+
+import theme from "../theme";
 
 const CoffeeList = () => {
   return (
     <FlatList
       data={coffees}
+      style={styles.container}
       renderItem={({ item }) => <CoffeeItem coffee={item} />}
       keyExtractor={(coffee) => coffee.id}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.listBackground,
+  },
+});
 
 export default CoffeeList;
