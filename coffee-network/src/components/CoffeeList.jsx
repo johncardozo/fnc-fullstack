0620da20d0f4953b098cbc4e9a9f9d80353cea68
John Cardozo <johncardozo@gmail.com>
2023-09-11 18:28:28 -0500

    React Native: Uso de ActivityIndicator para mostrar al usuario que hay un proceso en background

--- coffee-network/src/components/CoffeeList.jsx
+++ coffee-network/src/components/CoffeeList.jsx
@@ -1,4 +1,4 @@
-import { Text, FlatList, StyleSheet } from "react-native";
+import { Text, FlatList, StyleSheet, ActivityIndicator } from "react-native";
 
 import CoffeeItem from "./CoffeeItem";
 import theme from "../theme";
@@ -9,18 +9,29 @@ const CoffeeList = () => {
   const { coffees } = useCoffees();
 
   return (
-    <FlatList
-      data={coffees}
-      style={styles.container}
-      renderItem={({ item }) => <CoffeeItem coffee={item} />}
-      keyExtractor={(coffee) => coffee.id}
-    />
+    <>
+      {coffees ? (
+        <FlatList
+          data={coffees}
+          style={styles.container}
+          renderItem={({ item }) => <CoffeeItem coffee={item} />}
+          keyExtractor={(coffee) => coffee.id}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="red" style={styles.loading} />
+      )}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.listBackground,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
