21ba1f6e774855fd9712f7fcfc2e80a18fcd83ae
John Cardozo <johncardozo@gmail.com>
2023-09-11 18:03:19 -0500

    React Native: Obtiene los datos de la página principal de un endpoint remoto (mockapi)

--- coffee-network/src/components/CoffeeList.jsx
+++ coffee-network/src/components/CoffeeList.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import { Text, FlatList, StyleSheet } from "react-native";
 
-import { coffees } from "../data/coffees";
 import CoffeeItem from "./CoffeeItem";
-
 import theme from "../theme";
 
 const CoffeeList = () => {
+  const [coffees, setCoffees] = useState(null);
+
+  // Código ejecutado al montar el componente
+  useEffect(() => {
+    const fetchCoffees = async () => {
+      const url = "https://64f8eadd824680fd21803236.mockapi.io/coffees";
+
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setCoffees(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // Invoca la función para obtener los cafés del backend
+    fetchCoffees();
+  }, []);
+
   return (
     <FlatList
       data={coffees}
